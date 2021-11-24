1a0c7a685e26681793e2221a803732ddfa114ca1
Mehmet Emre ÖZ <nyz_memre_09@hotmail.com>
2021-11-25 01:40:10 +0300

    images added

---
+++ lib/utils/constant/lists.dart
@@ -0,0 +1,27 @@
+const List<String> images = [
+  "https://cdn.pixabay.com/photo/2018/12/10/21/33/winter-boots-3867775_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2015/09/04/23/28/boots-923189_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/01/06/13/24/womens-shoes-4745347_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/01/06/13/24/womens-shoes-4745343_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2015/11/05/22/33/sneakers-1024979_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/01/06/13/24/womens-shoes-4745344_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2018/08/31/09/35/baby-shoes-3644176_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2014/11/14/19/57/sneakers-531172_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2018/12/10/21/34/winter-boots-3867776_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/02/25/14/24/sports-4878991_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/01/06/13/24/womens-shoes-4745350_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2016/09/02/11/10/boots-1638873_960_720.jpg",
+];
+
+const List<String> gridImages=[
+  "https://cdn.pixabay.com/photo/2018/12/10/21/33/winter-boots-3867775_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/01/06/13/24/womens-shoes-4745347_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/01/06/13/24/womens-shoes-4745343_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/01/06/13/24/womens-shoes-4745344_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2018/08/31/09/35/baby-shoes-3644176_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2014/11/14/19/57/sneakers-531172_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2018/12/10/21/34/winter-boots-3867776_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/02/25/14/24/sports-4878991_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/01/06/13/24/womens-shoes-4745350_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2016/09/02/11/10/boots-1638873_960_720.jpg",
+];
